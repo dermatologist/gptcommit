c95a973034caf5624e47b053230dd06a2b622824
Bell Eapen <github_public@gulfdoctor.net>
2023-04-20 18:54:52 +0000

    update 3 files and create 1 file

--- src/gptcommit/gpt_commit.py
+++ src/gptcommit/gpt_commit.py
@@ -1,12 +1,109 @@
 import os
 import subprocess
 import sys
-from . import utils
-
+import utils
+import inquirer
 
 
 
 def gpt_commit():
     utils.is_git_repo()
-    diff = utils.get_diff(diff_per_file=True)
-    print(diff)
+
+
+    diff_per_file = False
+    commit_conventional = False
+    commit_language = "en"
+    commit_tokens = 75
+
+    # Check for flags
+    if len(sys.argv) > 1:
+        if "--help" in sys.argv:
+            print(
+                "Usage: python git_ai_commit.py [--diff-per-file] [--conventional] [--language <language>] [--choice <choice>]\n"
+            )
+            print("Flags:")
+            print("  --diff-per-file     Generate commit message per changed file")
+            print("  --conventional      Use conventional commit format")
+            print(
+                "  --language          Language for generated commit message (default: en)"
+            )
+            print(
+                "  --tokens            Choose number of tokens (default: 200)\n"
+            )
+            sys.exit(0)
+
+        flags = sys.argv[1:]
+
+        if "--diff-per-file" in flags:
+            diff_per_file = True
+
+        if "--conventional" in flags:
+            commit_conventional = True
+
+        if "--language" in flags:
+            language_index = flags.index("--language")
+            commit_language = flags[language_index + 1]
+
+        if "--tokens" in flags:
+            tokens_index = flags.index("--tokens")
+            commit_tokens = int(flags[tokens_index + 1])
+
+    diff = utils.get_diff(diff_per_file)
+
+    if not diff:
+        print(
+            "No staged changes found. Make sure there are changes and run `git add .`"
+        )
+        sys.exit(1)
+
+    # Accounting for GPT-3's input requirement of 4k tokens (approx 8k chars)
+    if len(diff) > 8000:
+        print(
+            "The diff is too large to write a commit message for. Please split your changes into multiple commits."
+        )
+        sys.exit(1)
+    selected_message = utils.get_commit_message(diff, commit_language, commit_tokens)
+
+    if commit_conventional:
+        conventional_choices = [
+            "feat",
+            "fix",
+            "docs",
+            "style",
+            "refactor",
+            "test",
+            "chore",
+        ]
+        questions = [
+            inquirer.List(
+                "conventional_choice",
+                message="What type of change are you making?",
+                choices=conventional_choices,
+            )
+        ]
+
+    print(f"Commit Message:\n{selected_message}")
+
+    confirmation_message = inquirer.prompt(
+        [
+            inquirer.List(
+                "use_commit_message",
+                message="Would you like to use this commit message?",
+                choices=["y", "n"],
+                default="y",
+            )
+        ]
+    )
+
+
+
+
+    if confirmation_message["use_commit_message"] == "n":
+        print("Commit message has not been committed.")
+        sys.exit(1)
+
+    subprocess.run(["git", "commit", "-m", f"{str(selected_message)}"])
+
+
+if __name__ == "__main__":
+    gpt_commit()
--- src/gptcommit/utils.py
+++ src/gptcommit/utils.py
@@ -1,5 +1,14 @@
 
 import subprocess
+# get transformers
+from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+import re
+
+# get large GPT2 tokenizer and GPT2 model
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2-large")
+GPT2 = TFGPT2LMHeadModel.from_pretrained(
+    "gpt2-large", pad_token_id=tokenizer.eos_token_id)
+whitelist = set('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 def is_git_repo() -> bool:
     try:
@@ -34,3 +43,17 @@ def get_diff(diff_per_file: bool) -> str:
         ).decode("utf-8")
 
     return diff_string
+
+
+def get_commit_message(prompt: str, language: str = "english", max_tokens=10) -> str:
+    # prompt = f"What follows '-------' is a git diff for a potential commit. Reply with an appropriate git commit message(a Git commit message should be concise but also try to describe the important changes in the commit) and don't include any other text but the message in your response. ------- {prompt}, language={language}"
+    # m = generator(prompt, max_new_tokens=max_tokens, num_return_sequences=1)
+    # print(m)
+    # return "Hello World"
+    # prompt = ''.join(filter(whitelist.__contains__, prompt))
+    prompt = re.sub('[^a-zA-Z]+', ' ', prompt)
+    print(prompt)
+    input_ids = tokenizer.encode(prompt, return_tensors='tf')
+    greedy_output = GPT2.generate(input_ids, max_length=max_tokens, num_return_sequences=1)
+    m = tokenizer.decode(greedy_output[0], skip_special_tokens=True)
+    return m
